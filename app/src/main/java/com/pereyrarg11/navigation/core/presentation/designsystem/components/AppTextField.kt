b2deef56f1d9f34d62b91e0f2cd7f15449112e4a
Gabriel Pereyra <pereyrarg11@gmail.com>
2025-07-12 16:49:39 -0600

    chore(Core): improve AppTextField

--- app/src/main/java/com/pereyrarg11/navigation/core/presentation/designsystem/components/AppTextField.kt
+++ app/src/main/java/com/pereyrarg11/navigation/core/presentation/designsystem/components/AppTextField.kt
@@ -1,19 +1,21 @@
 package com.pereyrarg11.navigation.core.presentation.designsystem.components
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.Lock
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
+import androidx.compose.foundation.text2.BasicTextField2
+import androidx.compose.foundation.text2.input.TextFieldLineLimits
+import androidx.compose.foundation.text2.input.TextFieldState
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -21,10 +23,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewLightDark
 import androidx.compose.ui.tooling.preview.PreviewParameter
@@ -33,79 +35,64 @@ import androidx.compose.ui.unit.dp
 import com.pereyrarg11.navigation.core.presentation.designsystem.AppTheme
 import com.pereyrarg11.navigation.core.presentation.tools.UiText
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun AppTextField(
-    value: String,
-    onValueChange: (String) -> Unit,
+    state: TextFieldState,
     label: UiText,
-    hint: UiText,
     modifier: Modifier = Modifier,
     errorMessage: UiText? = null,
-    isInputSecret: Boolean = false,
     keyboardType: KeyboardType = KeyboardType.Text,
 ) {
-    var isPasswordVisible by remember {
-        mutableStateOf(isInputSecret)
+    var isFocused by remember {
+        mutableStateOf(false)
     }
+    val inputShape = RoundedCornerShape(8.dp)
+    val errorMessageStr = errorMessage?.asString().orEmpty()
+
     Column(
         modifier = modifier,
     ) {
         Text(
             text = label.asString(),
             style = MaterialTheme.typography.bodyMedium,
+            color = MaterialTheme.colorScheme.onSurface,
         )
         Spacer(modifier = Modifier.height(6.dp))
-        OutlinedTextField(
-            value = value,
-            onValueChange = onValueChange,
-            modifier = Modifier.fillMaxWidth(),
-            visualTransformation = if (isPasswordVisible) {
-                PasswordVisualTransformation(mask = '*')
-            } else VisualTransformation.None,
-            colors = OutlinedTextFieldDefaults.colors(
-                focusedContainerColor = Color.Transparent,
-                unfocusedContainerColor = MaterialTheme.colorScheme.surface,
-                cursorColor = MaterialTheme.colorScheme.primary,
-                focusedBorderColor = MaterialTheme.colorScheme.primary,
-                unfocusedBorderColor = Color.Transparent,
-            ),
-            placeholder = {
-                Text(
-                    text = hint.asString(),
-                    style = MaterialTheme.typography.bodyLarge,
-                )
-            },
-            textStyle = MaterialTheme.typography.bodyLarge,
-            shape = RoundedCornerShape(10.dp),
-            trailingIcon = {
-                if (isInputSecret) {
-                    IconButton(
-                        onClick = {
-                            isPasswordVisible = !isPasswordVisible
-                        }
-                    ) {
-                        when {
-                            isPasswordVisible -> {
-                                Icon(
-                                    imageVector = Icons.Default.Lock,
-                                    contentDescription = "Hide password"
-                                )
-                            }
-
-                            !isPasswordVisible -> {
-                                Icon(
-                                    imageVector = Icons.Default.Check,
-                                    contentDescription = "Show password"
-                                )
-                            }
-                        }
-
+        BasicTextField2(
+            state = state,
+            modifier = Modifier
+                .fillMaxWidth()
+                .clip(inputShape)
+                .background(
+                    if (isFocused) {
+                        MaterialTheme.colorScheme.primary.copy(
+                            alpha = 0.05f,
+                        )
+                    } else {
+                        MaterialTheme.colorScheme.surface
                     }
-                }
-            },
+                )
+                .border(
+                    width = 1.dp,
+                    color = when {
+                        errorMessageStr.isNotEmpty() -> MaterialTheme.colorScheme.error
+                        else -> MaterialTheme.colorScheme.primary
+                    },
+                    shape = inputShape,
+                )
+                .padding(16.dp)
+                .onFocusChanged {
+                    isFocused = it.isFocused
+                },
+            textStyle = LocalTextStyle.current.copy(
+                color = MaterialTheme.colorScheme.onBackground,
+            ),
             keyboardOptions = KeyboardOptions(
                 keyboardType = keyboardType,
-            )
+            ),
+            lineLimits = TextFieldLineLimits.SingleLine,
+            cursorBrush = SolidColor(MaterialTheme.colorScheme.primary)
         )
         if (errorMessage != null) {
             Spacer(modifier = Modifier.height(6.dp))
@@ -118,25 +105,28 @@ fun AppTextField(
     }
 }
 
-class AppTextFieldParameterProvider : PreviewParameterProvider<Boolean> {
-    override val values: Sequence<Boolean>
-        get() = sequenceOf(false, true)
+class AppTextFieldErrorMessageProvider : PreviewParameterProvider<UiText?> {
+    override val values: Sequence<UiText?>
+        get() = sequenceOf(
+            null,
+            UiText.StaticString("Error message")
+        )
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Preview
 @PreviewLightDark
 @Composable
 private fun AppTextFieldPreview(
-    @PreviewParameter(AppTextFieldParameterProvider::class) isInputSecret: Boolean,
+    @PreviewParameter(AppTextFieldErrorMessageProvider::class) errorMessage: UiText?,
 ) {
     AppTheme {
-        AppTextField(
-            value = "Gabriel_",
-            onValueChange = {},
-            label = UiText.StaticString("Name"),
-            hint = UiText.StaticString("Your name"),
-            errorMessage = UiText.StaticString("Special characters are not allowed"),
-            isInputSecret = isInputSecret,
-        )
+        Surface {
+            AppTextField(
+                state = TextFieldState(initialText = "Text"),
+                label = UiText.StaticString("Label"),
+                errorMessage = errorMessage,
+            )
+        }
     }
 }
